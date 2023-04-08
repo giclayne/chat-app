980f954179e8ec2088c6ad8a51733b42d24f15b0
Giclayne <giclayne@gmail.com>
2023-04-08 13:34:48 -0300

    importacao das bibliotecas permitir a transferencia de dados entre o cliente e o servidor

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = 4000;
 
+const http = require('http').Server(app)
+const cors = require('cors')
+
+app.use(cors())
+
 app.get('/api', (req,res)=>{
     res.json({
         message: 'Hello World',
