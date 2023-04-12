62b5398583994242f7f1dec546ae14819f3789ff
Giclayne <giclayne@gmail.com>
2023-04-12 18:06:40 -0300

    atualizacao do chatBody para renderizar os dados da matriz de mensagens

--- client/src/components/ChatBody.js
+++ client/src/components/ChatBody.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const ChatBody = () => {
+const ChatBody = (messages) => {
     const navigate = useNavigate();
 
     const handleLeaveChat = () => {
@@ -20,20 +20,22 @@ const ChatBody = () => {
             </header>
 
             <div className="message__container">
-                <div className="message__chats">
-                    <p className="sender__name">Você</p>
-                    <div className="message__sender">
-                        <p>Olá</p>
-                    </div>
-                </div>
-
-                <div className="message__chats">
-                    <p>Outro</p>
-                    <div className="message__recipient">
-                        <p>Oi, tudo bem com você?</p>
-                    </div>
-                </div>
-
+                {messages.map ((message)=>
+                    message.name === localStorage.getItem('userName') ? (
+                        <div className="message__chats" key={message.id}>
+                            <p className="sender__name">Você</p>
+                            <div className="message__sender">
+                                <p>{message.text}</p>
+                            </div>
+                        </div>
+                    ): (
+                        <div className="message__chats" key={message.id}>
+                            <p>{message.name}</p>
+                            <div className="message__recipient">
+                                <p>{message.text}</p>
+                            </div>
+                        </div>
+                    ))}                            
                 <div className="message__status">
                     <p>Alguém está digitando...</p>
                 </div>
