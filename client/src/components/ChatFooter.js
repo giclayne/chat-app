7610a0146ec9d636131723c066f682e8cedd5da7
Giclayne <giclayne@gmail.com>
2023-04-12 17:44:34 -0300

    atualizacao da funcao para enviar a mensagem ao servidor

--- client/src/components/ChatFooter.js
+++ client/src/components/ChatFooter.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 
-const ChatFooter = () => {
+const ChatFooter = (socket) => {
     const [message, setMessage] = useState('');
 
     const handleSendMessage = (e) => {
         e.preventDefault();
-        console.log({ userName: localStorage.getItem('userName'), message });
+        if (message.trim() && localStorage.getItem('userName')) {
+          socket.emit('message', {
+            text: message,
+            name: localStorage.getItem('userName'),
+            id: `${socket.id}${Math.random()}`,
+            socketID: socket.id,
+          });
+        }
         setMessage('');
-    };
+      };
     return (
         <div className="chat__footer">
             <form className="form" onSubmit={handleSendMessage}>
