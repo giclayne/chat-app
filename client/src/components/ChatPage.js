c002a254742770935b1d9b4afec87a9822832439
Hester910 <hadassa.ester@ufrpe.br>
2023-04-12 19:27:48 -0300

    refatoracao da ChatPage

--- client/src/components/ChatPage.js
+++ client/src/components/ChatPage.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useRef} from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ChatBar from "./ChatBar";
 import ChatBody from "./ChatBody";
 import ChatFooter from "./ChatFooter";
@@ -7,24 +7,24 @@ const ChatPage = ({ socket }) => {
     const [messages, setMessages] = useState([]);
     const lastMessageRef = useRef(null);
     const [typingStatus, setTypingStatus] = useState('');
-  
+
     useEffect(() => {
         socket.on('messageResponse', (data) => setMessages([...messages, data]));
     }, [socket, messages]);
-  
+
     useEffect(() => {
         lastMessageRef.current.scrollIntoView({ behavior: 'smooth' });
-      }, [messages]);
-    
+    }, [messages]);
+
     useEffect(() => {
-    socket.on('typingResponse', (data) => setTypingStatus(data));
+        socket.on('typingResponse', (data) => setTypingStatus(data));
     }, [socket]);
 
     return (
         <div className="chat">
             <ChatBar socket={socket} />
-            <div className="chat__main">
-                <ChatBody 
+            <div className="chat_main">
+                <ChatBody
                     messages={messages}
                     lastMessageRef={lastMessageRef}
                     typingStatus={typingStatus} />
@@ -33,6 +33,6 @@ const ChatPage = ({ socket }) => {
         </div>
     );
 };
-  
+
 export default ChatPage;
 
