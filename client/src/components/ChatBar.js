694da8095d38821a31262f67675408278c8a7af5
Hester910 <hadassa.ester@ufrpe.br>
2023-04-12 19:28:57 -0300

    refatoracao da ChatBar

--- client/src/components/ChatBar.js
+++ client/src/components/ChatBar.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from "react";
 
-const ChatBar = ({socket}) => {
+const ChatBar = ({ socket }) => {
     const [users, setUsers] = useState([]);
     useEffect(() => {
         socket.on('newUserResponse', (data) => setUsers(data));
-      }, [socket, users]);
-        
+    }, [socket, users]);
+
     return (
-        <div className="chat__sidebar">
+        <div className="chat_sidebar">
             <h2>ChatMates</h2>
 
             <div>
-                <h4 className="chat__header">USUÁRIOS ONLINE</h4>
-                <div className="chat__users">
+                <h4 className="chat_header">USUÁRIOS ONLINE</h4>
+                <div className="chat_users">
                     {users.map((user) => (
-                    <p key={user.socketID}>{user.userName}</p>
+                        <p key={user.socketID}>{user.userName}</p>
                     ))}
                 </div>
             </div>
