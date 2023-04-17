0d8bc68517ed4ad8316885f5a69f74140e26766c
giclayne <75046706+giclayne@users.noreply.github.com>
2023-04-17 14:56:02 -0300

    Revert "Revert "Merge""

--- client/src/components/ChatFooter.js
+++ client/src/components/ChatFooter.js
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
 
-const ChatFooter = () => {
+const ChatFooter = ({socket}) => {
     const [message, setMessage] = useState('');
 
+    const handleTyping = () => {
+      let user = localStorage.getItem("userName");
+      socket.emit("typing", `${user} está digitando...`);
+  };
     const handleSendMessage = (e) => {
         e.preventDefault();
-        console.log({ userName: localStorage.getItem('userName'), message });
+        if (message.trim() && localStorage.getItem('userName')) {
+          socket.emit('message', {
+            text: message,
+            name: localStorage.getItem('userName'),
+            id: `${socket.id}${Math.random()}`,
+            socketID: socket.id,
+          });
+        }
         setMessage('');
-    };
+      };
     return (
         <div className="chat__footer">
             <form className="form" onSubmit={handleSendMessage}>
@@ -17,6 +28,7 @@ const ChatFooter = () => {
                     className="message"
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}
+                    onKeyDown={handleTyping}
                 />
                 <button className="sendBtn">ENVIAR</button>
             </form>
